2d7029c0a14ea27b3563473cbc80fab16a84ad05
Chunyang Xu <xuchunyang56@gmail.com>
2014-05-26 01:41:10 +0800

    notmain: Update for testing

--- src/libbiji/editor/notmain.c
+++ src/libbiji/editor/notmain.c
@@ -9,6 +9,7 @@ button_cb (GtkWidget *button, gpointer user_data)
   // biji_webkit2_editor_cut (view);
   // biji_webkit2_editor_apply_format (editor, BIJI_STRIKE);
   biji_webkit2_editor_get_selection (editor);
+  // biji_webkit2_editor_apply_format_new (editor, "bold");
 }
 
 int
@@ -23,6 +24,7 @@ main (int   argc,
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title (GTK_WINDOW (window), "Biji WebKit2 Editor");
+  gtk_window_set_default_size (window, 480, 640);
 
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
@@ -34,7 +36,7 @@ main (int   argc,
   g_object_set_data ((GObject*)window, "webkit2-editor", editor);
   gtk_box_pack_start (GTK_BOX (box), GTK_WIDGET (WEBKIT_WEB_VIEW (editor)), TRUE, TRUE, 0);
 
-  button = gtk_button_new_with_label ("copy");
+  button = gtk_button_new_with_label ("test button");
   g_signal_connect (button, "clicked", G_CALLBACK (button_cb), window);
   gtk_box_pack_start (GTK_BOX (box), button, TRUE, TRUE, 0);
 
