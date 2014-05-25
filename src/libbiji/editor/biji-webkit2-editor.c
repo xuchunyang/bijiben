018b8f5df11bf47c44b2544038d4433520ca0528
Chunyang Xu <xuchunyang56@gmail.com>
2014-05-25 21:51:54 +0800

    WebKit2: get selected html via js

--- src/libbiji/editor/biji-webkit2-editor.c
+++ src/libbiji/editor/biji-webkit2-editor.c
@@ -17,7 +17,11 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <JavaScriptCore/JSStringRef.h>
+#include <JavaScriptCore/JSValueRef.h>
+
 #include "biji-webkit2-editor.h"
+
 
 struct _BijiWebkit2EditorPrivate
 {
@@ -28,10 +32,6 @@ struct _BijiWebkit2EditorPrivate
   gulong color_changed;
   WebKitSettings* settings;
 };
-
-
-
-
 
 
 G_DEFINE_TYPE (BijiWebkit2Editor, biji_webkit2_editor, WEBKIT_TYPE_WEB_VIEW);
@@ -70,8 +70,6 @@ biji_webkit2_editor_constructed (GObject *obj)
                              "<html><head><style>body {color:blue}</style></head>"
                              "<body contentEditable='true'>Hello, world!</body></html>",
                              NULL);
-
-
 }
 
 static void
@@ -147,16 +145,97 @@ biji_webkit2_editor_apply_format (BijiWebkit2Editor *self, gint format)
   g_free(script);
 }
 
+static void
+web_view_javascript_finished (GObject      *object,
+                              GAsyncResult *result,
+                              gpointer      user_data)
+{
+  WebKitJavascriptResult *js_result;
+  JSValueRef              value;
+  JSGlobalContextRef      context;
+  GError                 *error = NULL;
+
+  js_result = webkit_web_view_run_javascript_finish (WEBKIT_WEB_VIEW (object),
+                                                     result,
+                                                     &error);
+  if (!js_result)
+    {
+      g_warning ("Error running javascript: %s", error->message);
+      g_error_free (error);
+      return;
+    }
+
+  context = webkit_javascript_result_get_global_context (js_result);
+  value = webkit_javascript_result_get_value (js_result);
+  if (JSValueIsString (context, value))
+    {
+      JSStringRef js_str_value;
+      gchar      *str_value;
+      gsize       str_length;
+
+      js_str_value = JSValueToStringCopy (context, value, NULL);
+      str_length = JSStringGetMaximumUTF8CStringSize (js_str_value);
+      str_value = (gchar *)g_malloc (str_length);
+      JSStringGetUTF8CString (js_str_value, str_value, str_length);
+      JSStringRelease (js_str_value);
+      g_print ("Script result: %s\n", str_value); /* FIXME: return str_value */
+      g_free (str_value);
+    }
+  else
+    {
+      g_warning ("Error running javascript: unexpected return value");
+    }
+
+  webkit_javascript_result_unref (js_result);
+}
+
+static void
+web_view_get_selected_html (WebKitWebView *web_view)
+{
+  gchar *script;
+  script = g_strdup_printf ("function getSelectionHtml() {"
+                            "    var html = '';"
+                            "    if (typeof window.getSelection != 'undefined') {"
+                            "        var sel = window.getSelection();"
+                            "        if (sel.rangeCount) {"
+                            "            var container = document.createElement('div');"
+                            "            for (var i = 0, len = sel.rangeCount; i < len; ++i) {"
+                            "                container.appendChild(sel.getRangeAt(i).cloneContents());"
+                            "            }"
+                            "            html = container.innerHTML;"
+                            "        }"
+                            "    } else if (typeof document.selection != 'undefined') {"
+                            "        if (document.selection.type == 'Text') {"
+                            "            html = document.selection.createRange().htmlText;"
+                            "        }"
+                            "    }"
+                            "    return html;"
+                            "}"
+                            "getSelectionHtml()");
+  webkit_web_view_run_javascript (web_view,
+                                  script,
+                                  NULL,
+                                  web_view_javascript_finished,
+                                  NULL);
+
+  g_free (script);
+}
+
 gboolean
 biji_webkit2_editor_has_selection (BijiWebkit2Editor *self)
 {
-  /* TODO: Add public function implementation here */
+  if (biji_webkit2_editor_get_selection (self) == "")
+    return false;
+  else
+    return true;
 }
 
 gchar *
 biji_webkit2_editor_get_selection (BijiWebkit2Editor *self)
 {
-  /* TODO: Add public function implementation here */
+  /* FIXME: */
+  web_view_get_selected_html (WEBKIT_WEB_VIEW (self));
+  return "";
 }
 
 void
